133216d552d287bbbe491292164b881aefe59db2
Yong-Dd <71385485+Yong-Dd@users.noreply.github.com>
2020-10-31 22:13:57 +0900

    html

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 #utube
 
 cloning Youtube with vanillaJs and NodeJs
+
+##pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
+  
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,8 +8,8 @@ import routes from "../routes.js";
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 //The meaning of this is that it all exprot
