006d157def9884b8b81c172b667c7fe2bc962279
Yong-Dd <71385485+Yong-Dd@users.noreply.github.com>
2020-11-11 22:29:22 +0900

    edit video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,24 @@
 import express from "express";
-import { deleteVideo, editVideo, getUpload, postUpload, VideoDetail, videos } from "../controller/videoController.js";
+import { deleteVideo, getUpload, getEditVideo, postEditVideo, postUpload, VideoDetail, videos } from "../controller/videoController.js";
 import { uploadVideo } from "../middlewares.js";
 import routes from "../routes.js";
 
 const videoRouter = express.Router();
 
+//upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.videos, videos);
+//video detail
 videoRouter.get(routes.videoDetail(), VideoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+//Edit video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//delete video
 videoRouter.get(routes.deleteVideo, deleteVideo);
+
+//?
+videoRouter.get(routes.videos, videos);
 export default videoRouter;
